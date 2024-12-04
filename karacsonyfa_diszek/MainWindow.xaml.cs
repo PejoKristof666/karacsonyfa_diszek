9e027cd04208e8cde38e0228bad5cf77be6336cd
Péjó Kristóf <pejo.kristof.21i@egressy.info>
2024-12-04 11:34:35 +0100

    hehehehe

--- karacsonyfa_diszek/MainWindow.xaml.cs
+++ karacsonyfa_diszek/MainWindow.xaml.cs
@@ -24,5 +24,30 @@ namespace karacsonyfa_diszek
         {
             InitializeComponent();
         }
+
+        void createStoreItem(disz oneDisz)
+        {
+            Border oneBorder = new Border();
+            Store.Children.Add(oneBorder);
+            Grid oneGrid = new Grid();
+            oneBorder.Child = oneGrid;
+
+            RowDefinition firstRow = new RowDefinition();
+            RowDefinition secondRow = new RowDefinition();
+            RowDefinition thirdRow = new RowDefinition();
+            RowDefinition fourthRow = new RowDefinition();
+            oneGrid.RowDefinitions.Add(firstRow);
+            oneGrid.RowDefinitions.Add(secondRow);
+            oneGrid.RowDefinitions.Add(thirdRow);
+            oneGrid.RowDefinitions.Add(fourthRow);
+            TextBlock name = new TextBlock();
+            TextBlock price = new TextBlock();
+            TextBlock count = new TextBlock();
+            oneGrid.Children.Add(name);
+            oneGrid.Children.Add(price);
+            oneGrid.Children.Add(count);
+            Button BuyButton = new Button();
+            oneGrid.Children.Add(BuyButton);
+        }
     }
 }
---
+++ karacsonyfa_diszek/disz.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace karacsonyfa_diszek
+{
+    class disz
+    {
+        public string name { get; set; }
+        public int price { get; set; }
+        public int stock { get; set; }
+
+    }
+}
